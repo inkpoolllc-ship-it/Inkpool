37e1a5340ac6502cf32436d0f28b8bee62146ce5
Ink Pool <inkpoolllc@gmail.com>
2025-10-26 18:46:36 -0500

    Add stripe configuration file

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,9 +1,7 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2024-06-20',
+  apiVersion: '2025-09-30.clover',
 })
 
 export default stripe
-
-
